d0782336002968c1e64b257ce3ee3bffe187902a
Joona Hoikkala <joona@kuori.org>
2018-11-12 19:47:49 +0200

    Added regexp filter / matcher

--- pkg/filter/filter.go
+++ pkg/filter/filter.go
@@ -16,5 +16,8 @@ func NewFilterByName(name string, value string) (ffuf.FilterProvider, error) {
 	if name == "word" {
 		return NewWordFilter(value)
 	}
+	if name == "regexp" {
+		return NewRegexpFilter(value)
+	}
 	return nil, fmt.Errorf("Could not create filter with name %s", name)
 }
