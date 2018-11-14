0015f37c4294ac0c0bc04d968c44ee7b5d94ae2e
Joona Hoikkala <joona@kuori.org>
2018-11-15 00:18:43 +0200

    Get rid of the last external dependency

---
+++ pkg/ffuf/multierror.go
@@ -0,0 +1,30 @@
+package ffuf
+
+import (
+	"fmt"
+)
+
+type Multierror struct {
+	errors []error
+}
+
+//NewMultierror returns a new Multierror
+func NewMultierror() Multierror {
+	return Multierror{}
+}
+
+func (m *Multierror) Add(err error) {
+	m.errors = append(m.errors, err)
+}
+
+func (m *Multierror) ErrorOrNil() error {
+	var errString string
+	if len(m.errors) > 0 {
+		errString += fmt.Sprintf("%d errors occured.\n", len(m.errors))
+		for _, e := range m.errors {
+			errString += fmt.Sprintf("\t* %s\n", e)
+		}
+		return fmt.Errorf("%s", errString)
+	}
+	return nil
+}
