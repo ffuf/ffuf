4485cf88e630301de896c7e7f25d8ac98be2239c
Tapio Vuorinen <delic@imordnilap.net>
2019-06-26 15:17:00 +0300

    allow ranges on http status matching/filtering

--- pkg/filter/status.go
+++ pkg/filter/status.go
@@ -8,33 +8,35 @@ import (
 	"github.com/ffuf/ffuf/pkg/ffuf"
 )
 
+const AllStatuses = 0
+
 type StatusFilter struct {
-	Value []int64
+	Value []ValueRange
 }
 
 func NewStatusFilter(value string) (ffuf.FilterProvider, error) {
-	var intvals []int64
+	var intranges []ValueRange
 	for _, sv := range strings.Split(value, ",") {
 		if sv == "all" {
-			intvals = append(intvals, 0)
+			intranges = append(intranges, ValueRange{AllStatuses, AllStatuses})
 		} else {
-			intval, err := strconv.ParseInt(sv, 10, 0)
+			vr, err := ValueRangeFromString(sv)
 			if err != nil {
-				return &StatusFilter{}, fmt.Errorf("Status filter or matcher (-fc / -mc): invalid value %s", value)
+				return &StatusFilter{}, fmt.Errorf("Status filter or matcher (-fc / -mc): invalid value %s", sv)
 			}
-			intvals = append(intvals, intval)
+			intranges = append(intranges, vr)
 		}
 	}
-	return &StatusFilter{Value: intvals}, nil
+	return &StatusFilter{Value: intranges}, nil
 }
 
 func (f *StatusFilter) Filter(response *ffuf.Response) (bool, error) {
 	for _, iv := range f.Value {
-		if iv == 0 {
+		if iv.min == AllStatuses && iv.max == AllStatuses {
 			// Handle the "all" case
 			return true, nil
 		}
-		if iv == response.StatusCode {
+		if iv.min <= response.StatusCode && response.StatusCode <= iv.max {
 			return true, nil
 		}
 	}
@@ -44,10 +46,12 @@ func (f *StatusFilter) Filter(response *ffuf.Response) (bool, error) {
 func (f *StatusFilter) Repr() string {
 	var strval []string
 	for _, iv := range f.Value {
-		if iv == 0 {
+		if iv.min == AllStatuses && iv.max == AllStatuses {
 			strval = append(strval, "all")
+		} else if iv.min == iv.max {
+			strval = append(strval, strconv.Itoa(int(iv.min)))
 		} else {
-			strval = append(strval, strconv.Itoa(int(iv)))
+			strval = append(strval, strconv.Itoa(int(iv.min))+"-"+strconv.Itoa(int(iv.max)))
 		}
 	}
 	return fmt.Sprintf("Response status: %s", strings.Join(strval, ","))
