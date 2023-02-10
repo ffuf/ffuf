63e8cb3513c81ff40f0091356d6de9982caf8c2b
li <1312513963@qq.com>
2023-02-11 00:11:24 +0800

    fix: output overwrite problem

--- pkg/output/stdout.go
+++ pkg/output/stdout.go
@@ -405,6 +405,7 @@ func (s *Stdoutput) prepareInputsOneLine(res ffuf.Result) string {
 }
 
 func (s *Stdoutput) resultQuiet(res ffuf.Result) {
+	fmt.Print("\x1b[1K \x1b[100D")
 	fmt.Println(s.prepareInputsOneLine(res))
 }
 
@@ -440,11 +441,13 @@ func (s *Stdoutput) resultMultiline(res ffuf.Result) {
 			}
 		}
 	}
+	fmt.Print("\x1b[1K \x1b[100D")
 	fmt.Printf("%s\n%s\n", res_hdr, reslines)
 }
 
 func (s *Stdoutput) resultNormal(res ffuf.Result) {
 	resnormal := fmt.Sprintf("%s%s%-23s [Status: %d, Size: %d, Words: %d, Lines: %d, Duration: %dms]%s", TERMINAL_CLEAR_LINE, s.colorize(res.StatusCode), s.prepareInputsOneLine(res), res.StatusCode, res.ContentLength, res.ContentWords, res.ContentLines, res.Duration.Milliseconds(), ANSI_CLEAR)
+	fmt.Print("\x1b[1K \x1b[100D")
 	fmt.Println(resnormal)
 }
 
@@ -453,6 +456,7 @@ func (s *Stdoutput) resultJson(res ffuf.Result) {
 	if err != nil {
 		s.Error(err.Error())
 	} else {
+		fmt.Print("\x1b[1K \x1b[100D")
 		fmt.Fprint(os.Stderr, TERMINAL_CLEAR_LINE)
 		fmt.Println(string(resBytes))
 	}
